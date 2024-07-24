651b7521a7c64583ca17241c0401f9ed129911cc
채향석/기반플랫폼개발팀 <neochae@lguplus.co.kr>
2024-07-24 20:07:47 +0900

    Add remove for .git hidden

--- actions-clean-1.0/entrypoint.sh
+++ actions-clean-1.0/entrypoint.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 
 # 작업 디렉토리 초기화
-rm -rf "${GITHUB_WORKSPACE:?}"/*
+rm -rf "${GITHUB_WORKSPACE:?}"/* "${GITHUB_WORKSPACE:?}/.git*"
 
 if test "${RUNNER_DEBUG:-0}" != '1'; then
   set +o xtrace
